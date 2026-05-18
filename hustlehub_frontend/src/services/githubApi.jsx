44d16b3dfea1a32edaef0c6467cff5bd75c5eb63
Yasmin101-101 <yasminmohamudyasmin@gmail.com>
2026-05-18 20:46:15 +0300

    HU-47 Improve Mentors Page UI/Search

--- hustlehub_frontend/src/services/githubApi.jsx
+++ hustlehub_frontend/src/services/githubApi.jsx
@@ -1,26 +1,21 @@
-const githubUsers = [
-  "torvalds",
-  "gaearon",
-  "yyx990803",
-  "kentcdodds",
-  "tj",
-];
+async function getMentors(query = "javascript developer") {
+  const response = await fetch(
+    `https://api.github.com/search/users?q=${encodeURIComponent(query)}&per_page=12`
+  );
+  const data = await response.json();
 
-async function getMentors() {
-  const mentors = [];
+  if (!data.items) return [];
 
-  for (const username of githubUsers) {
-    const response = await fetch(`https://api.github.com/users/${username}`);
-    const data = await response.json();
-
-    console.log("GitHub user data:", data);
-
-    if (data && data.login && data.avatar_url) {
-      mentors.push(data);
-    }
-  }
-
-  return mentors;
+  return data.items.map((user) => ({
+    id: user.id,
+    login: user.login,
+    name: user.login,
+    avatar_url: user.avatar_url,
+    bio: "Visit profile to learn more.",
+    public_repos: "N/A",
+    followers: "N/A",
+    html_url: user.html_url,
+  }));
 }
 
 export default getMentors;
